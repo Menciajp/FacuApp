f3c5af9cbf3f5c7c5e0ccfb65286046d15558c4d
Menciajp <menciajp.27@gmail.com>
2023-08-03 19:26:21 -0300

    3/8/2023

--- src/main/java/UIcontrollers/MenuController.java
+++ src/main/java/UIcontrollers/MenuController.java
@@ -1,14 +1,66 @@
 package UIcontrollers;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import java.awt.event.ActionEvent;
 import java.io.IOException;
 
 public class MenuController {
     private Stage stage;
+    @FXML
+    private Button btn_crear;
+
+    @FXML
+    private Button btn_creelo;
+
+    @FXML
+    private Button btn_docente;
+
+    @FXML
+    private Button btn_institutos;
+
+    @FXML
+    private Button btn_volver;
+
+    @FXML
+    private Button btn_volverMenu;
+
+    @FXML
+    private MenuButton mb_institutos;
+
+    @FXML
+    private Pane pn_crearInstituto;
+
+    @FXML
+    private Pane pn_instSelec;
+
+    @FXML
+    private Pane pn_seleccion;
+
+    @FXML
+    private TextField tf_crearInst;
+
+    public void cambioPantalla(ActionEvent event){
+        if(event.getSource() == btn_volver || event.getSource() ==btn_volverMenu){
+            pn_crearInstituto.setVisible(false);
+            pn_instSelec.setVisible(false);
+            pn_seleccion.setVisible(true);
+        }else if (event.getSource() == btn_institutos){
+            pn_instSelec.setVisible(true);
+            pn_seleccion.setVisible(false);
+        }else if(event.getSource() == btn_creelo){
+            pn_instSelec.setVisible(false);
+            pn_crearInstituto.setVisible(true);
+        }
+    }
     private void changeScene(String fxml) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource(fxml));
 
@@ -27,7 +79,6 @@ public class MenuController {
         stage.show();
         stage.centerOnScreen();
     }
-
     public void setStage(Stage stage) {
         this.stage = stage;
     }
